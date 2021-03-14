032bcb27e9787eb84fe1c9e40ff783a112c541f1
jsamyn <joseph.j.samyn@gmail.com>
2021-03-13 21:05:21 -0700

    initial commit

---
+++ app/src/main/java/com/joesamyn/envelope/ui/fragment/Home.kt
@@ -0,0 +1,34 @@
+package com.joesamyn.envelope.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joesamyn.envelope.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Home : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         * @return A new instance of fragment Home.
+         */
+        @JvmStatic
+        fun newInstance(): Home {
+            return Home()
+        }
+    }
+}
