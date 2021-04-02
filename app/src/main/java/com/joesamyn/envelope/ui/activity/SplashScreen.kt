efc43b0f2bcf65f98cd77e2ad6afcda65be72d9e
Joseph Samyn <38860339+Jsamyn@users.noreply.github.com>
2021-04-02 09:32:45 -0700

    Added splash screen.

---
+++ app/src/main/java/com/joesamyn/envelope/ui/activity/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.joesamyn.envelope.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.joesamyn.envelope.R
+
+class SplashScreen : AppCompatActivity() {
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
